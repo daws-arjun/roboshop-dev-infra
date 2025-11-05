0ec9c2ba9445c8c7ffe7497f6406814ae54dc149
daws-arjun <a.nagarjuna97@gmail.com>
2025-11-05 16:40:03 +0530

    latest

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "bastion" {
     subnet_id = local.public_subnet_id
     iam_instance_profile = aws_iam_instance_profile.bastion.name
     
-     # need more for terraform
+     # need more storage for terraform
     root_block_device {
         volume_size = 50
         volume_type = "gp3" # or "gp2", depending on your preference
--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z06547222JC0EDVX2ID1B"
+}
+
+variable "domain_name" {
+    default = "anuarjun.space"
+}
